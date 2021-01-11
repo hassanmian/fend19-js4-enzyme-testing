da8f9c7baa358888853274087274eaa554d8b89b
Hassan Mian <mian@willandskill.se>
2021-01-11 11:03:59 +0100

    Add Heading to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Heading from './components/Heading';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Heading heading="Hello" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
